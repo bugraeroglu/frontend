c8906acd9d95ca9ec35a6d950d79a0a4458e580b
zeynepzor <123664199+zeynepzor@users.noreply.github.com>
2025-11-14 15:13:47 +0300

    feat(SCRUM-31): Add Order History and Order Detail pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
-import Products from "./pages/Products";        // dosya adın nasılsa ona göre
+import Products from "./pages/Products";
 import ProductDetail from "./pages/ProductDetail";
+import OrderHistory from "./pages/OrderHistory";
+import OrderDetail from "./pages/OrderDetail";
 
 export default function App() {
   return (
@@ -12,9 +14,13 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/orders" element={<OrderHistory />} />
+        <Route path="/orders/:id" element={<OrderDetail />} />
+        
         {/* 404 */}
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );
 }
+
