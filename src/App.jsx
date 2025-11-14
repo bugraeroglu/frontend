88c1ed90d92b83db434f4fe8b673653cf67ed4c1
Zeynep Dağcı <zeynepdagci@Zeyneps-MacBook-Pro-5.local>
2025-11-14 14:21:02 +0300

    Added Navbar, Cart page, cart functionality, UI improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
 // src/App.jsx
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
-import Products from "./pages/Products";        // dosya adın nasılsa ona göre
+import Products from "./pages/Products";
 import ProductDetail from "./pages/ProductDetail";
+import Cart from "./pages/Cart";
+
+
+function AppContent() {
+  const location = useLocation();
+
+  return (
+    <>
+      {/* Login sayfasında navbar görünmesin */}
+      {location.pathname !== "/login" && <Navbar />}
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+
+      </Routes>
+    </>
+  );
+}
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetail />} />
-        {/* 404 */}
-        <Route path="*" element={<Navigate to="/login" replace />} />
-      </Routes>
+      <AppContent />
     </Router>
   );
 }
